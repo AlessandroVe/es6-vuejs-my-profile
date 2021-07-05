057125aa332bf5dda1dde696137e55a76dc1e791
AlessandroVe <aveneziano536@gmail.com>
2021-07-05 16:12:09 +0200

    .

--- js/script-vue.js
+++ js/script-vue.js
@@ -9,17 +9,20 @@ new Vue(
                     name: 'Pierfilippo',
                     surname: 'Baudo',
                     pic: 'https://www.focus.it/site_stored/imgs/0003/042/nerd2.1020x680.jpg',
+                    newTextPost:"",
                 },
                 posts: [
                     {
                         text: "C'è nessuno?",
                         mediaPath: 'https://acqualete.it/wp-content/uploads/2015/09/La-Particella-di-Sodio-1.jpg',
                         date: '26-05-2021'
-                    }, {
+                    }, 
+                    {
                         text: "Vi ricordate di Windows XP? Che ricordi, non ce la faccio...",
                         mediaPath: 'https://upload.wikimedia.org/wikipedia/it/d/d3/Colline_%28immagine%29.jpg',
                         date: '01-06-2021'
-                    }, {
+                    }, 
+                    {
                         text: "Mi sono iscritto in palestra!",
                         date: '16-06-2021'
                     },
@@ -31,5 +34,22 @@ new Vue(
                 ]
             }
         },
+        created(){
+
+    
+        },
+        mounted(){
+            
+        },
+        computed: {
+
+        },
+        methods:{
+            newPost : function(){
+                console.log(this.newTextPost)
+            }
+           
+        }
+    
     }
 );
